001c4847d1ad0f137fcec5c55056f88de564da22
mark <1210917553@qq.com>
2022-07-13 22:23:42 +0800

    chore: handwritting

---
+++ src/01.new.js
@@ -0,0 +1,21 @@
+
+function myNew(fn, ...args) {
+  let obj = Object.create(fn.prototype)
+  let res = fn.call(obj, ...args)
+  if (res && (typeof res === 'object' || typeof res === 'function')) {
+    return res
+  }
+  return obj
+}
+
+// test
+function Person(name, age) {
+  this.name = name
+  this.age = age
+}
+Person.prototype.getPersonInfo = function() {
+  console.log(`My name is ${this.name}, i am ${this.age}!`)
+}
+let person = myNew(Person, 'Mark', 23)
+console.log(person);
+person.getPersonInfo()
