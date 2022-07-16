465237c701527ed4d6f988b207591e3c8e857019
mark <1210917553@qq.com>
2022-07-16 23:34:41 +0800

    chore: handwritting

--- src/03.promise.js
+++ src/03.promise.js
@@ -3,10 +3,20 @@ const PENDING = 'PENDING'
 const FULFILLED = 'FULFILLED'
 const REJECTED = 'REJECTED'
 
-class MyPromise {
+function resolvePromise(promise, p, resolve, reject) {
+  if (promise === p) {
+    return reject(new TypeError("self promise"))
+  }
+  if (p instanceof MyPromise) {
+    p.then(resolve, reject)
+  } else {
+    resolve(p)
+  }
+}
 
-	constructor(exec) {
-		exec(this.resolve, this.reject)
+class MyPromise {
+  constructor(exec) {
+    exec(this.resolve, this.reject)
   }
 
   status = PENDING
@@ -14,48 +24,72 @@ class MyPromise {
   value = null
   reason = null
 
-  onfulfilled = null
-  onrejected = null
+  onfulfilledCallbacks = []
+  onrejectedCallbacks = []
 
-	resolve = (value) => {
-		if (this.status === PENDING) {
+  resolve = (value) => {
+    if (this.status === PENDING) {
       this.status = FULFILLED
       this.value = value
-      this.onfulfilled && this.onfulfilled(value)
+      while (this.onfulfilledCallbacks.length) {
+        this.onfulfilledCallbacks.shift()(value)
+      }
     }
   }
 
   reject = (reason) => {
-		if (this.status === PENDING) {
+    if (this.status === PENDING) {
       this.status = REJECTED
       this.reason = reason
-      this.onrejected && this.onrejected(reason)
+      while (this.onrejectedCallbacks.length) {
+        this.onrejectedCallbacks.shift()(reason)
+      }
     }
   }
 
   then(onfulfilled, onrejected) {
-    if (this.status === FULFILLED) {
-			onfulfilled(this.value)
-    } else if (this.status === REJECTED) {
-      onrejected(this.reason)
-    } else if (this.status === PENDING) {
-      this.onfulfilled = onfulfilled
-      this.onrejected = onrejected
-    }
+    const promise = new MyPromise((resolve, reject) => {
+      if (this.status === FULFILLED) {
+        queueMicrotask(() => {
+          const p = onfulfilled(this.value)
+          resolvePromise(promise, p, resolve, reject)
+        })
+      } else if (this.status === REJECTED) {
+        onrejected(this.reason)
+      } else if (this.status === PENDING) {
+        this.onfulfilledCallbacks.push(onfulfilled)
+        this.onrejectedCallbacks.push(onrejected)
+      }
+    })
+    return promise
   }
-
 }
 
 // test
+function otherPromise() {
+  return new MyPromise((resolve, reject) => {
+    resolve('other promise resolve')
+  })
+}
 let p = new MyPromise((resolve, reject) => {
-  setTimeout(() => {
-		resolve('timeout')
-  }, 2000)
-	// resolve('success')
+  // setTimeout(() => {
+  //   resolve('timeout')
+  // }, 2000)
+  resolve('success')
   // reject('error')
 })
-p.then(res => {
+// let p2 = p.then((res) => {
+//   console.log(res)
+//   return p2
+// })
+// p2.then(res => {
+//   console.log(rs);
+// }, reason => {
+//   console.log(reason);
+// })
+p.then((res) => {
   console.log(res)
-}, err => {
-  console.log(err)
+  return otherPromise()
+}).then((res) => {
+  console.log(res)
 })
